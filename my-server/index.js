2862357d1bbc96b64eb49d43f6f4334f291cc895
Rajeevssjava <83974312+Rajeevssjava@users.noreply.github.com>
2025-07-25 16:17:44 +0530

    Update index.js

--- my-server/index.js
+++ my-server/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = 'https://frontend-lxkz.onrender.com';
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors());
